98f1a54639b34b74bd8edd158ff46d665cb9bf1f
unknown <(Dell-PC@DELL.(none)>
2022-05-31 21:45:06 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+resource "aws_security_group" "sg" {
+  name        = "webserver"
+  vpc_id      = "vpc-01fb96c99910fe3cf"
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [ "10.0.0.0/24" ]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "hfhj"
+    User = "princy"
+  }
+}
